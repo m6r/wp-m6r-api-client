b95c9e5d3856ec09603d42362a3af048db209923
Guillaume Robin <dev@m6r.fr>
2015-02-14 18:14:04 +0100

    add variable for wordpress SEO

--- m6r-api.php
+++ m6r-api.php
@@ -3,7 +3,7 @@
 * Plugin Name: M6R API
 * Plugin URI: https://github.com/m6r/
 * Description: Plugin fournissant des shortcodes pour afficher des résultats de l'API (m6r.fr/api). Shortcodes actuellement disponibles : [m6r-signatures]: nombre de signatures
-* Version: 0.0.1
+* Version: 0.0.2
 * Author: Guillaume Robin
 * Author URI: http://guillaumerobin.github.io
 * License: AGPL3
@@ -19,7 +19,11 @@ function m6r_api_signatures( $atts ){
     } catch (Error $e) {
         return '';
     }
-
     return $data->value;
 }
-add_shortcode('m6r-signatures', 'm6r_api_signatures');
+add_shortcode('m6r-signatures', 'm6r_api_signatures' );
+
+function register_m6r_api_extra_replacements() {
+    wpseo_register_var_replacement( 'm6r-signatures', 'm6r_api_signatures', 'basic  ', 'Nombre de signatures du M6R.' );
+}
+add_action('wpseo_register_extra_replacements', 'register_m6r_api_extra_replacements');
